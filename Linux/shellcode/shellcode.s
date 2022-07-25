2bd89f80dbea4f559fa74c9fc57065a068aeb841
juzzrespecter <danisarp@gmail.com>
2022-07-25 21:23:31 +0200

    Linux: new shellcode

--- Linux/shellcode/shellcode.s
+++ Linux/shellcode/shellcode.s
@@ -6,9 +6,9 @@ _shellcode:
 	pop	ebx		; referencia a "gnome-calculatorA"
 	mov	[ebx+16],al	; "gnome-calculator\0"
 	mov	[ebx+17],ebx	; "gnome-calculator\0"&"gnome-calculator\0"
-	mov	[ebx+32],eax	; "gnome-calculator\0"&"gnome-calculator\0"\0\0\0\0
+	mov	[ebx+35],eax	; "gnome-calculator\0"&"gnome-calculator\0"[...]\0\0\0\0
 	lea	ecx,[ebx+16]	; cargamos [ebx+16] como segundo argumento
-	lea	edx,[ebx+32]	; cargamos [ebx+32] como tercer argumento
+	lea	edx,[ebx+35]	; cargamos [ebx+32] como tercer argumento
 	mov	al,11		; execve
 	int	0x80		; call
 	mov	al,1
