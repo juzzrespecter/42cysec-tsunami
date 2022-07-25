2af6f0e63cbada4874457043dc402a7f3a352caf
juzzrespecter <danisarp@gmail.com>
2022-07-25 23:58:52 +0200

    backup

--- Linux/shellcode/shellcode.s
+++ Linux/shellcode/shellcode.s
@@ -1,12 +1,12 @@
 	section	.text
 	global main
 main:
-	jmp	_shellcode_get_cmd
-_shellcode:
-	pop	ebx		; referencia a "gnome-calculatorA"
+	jmp	_shellcd_db
+_shellcd:	
 	xor	eax,eax
-	mov	[ebx+16],al	; "gnome-calculator\0"
-	mov	[ebx+17],ebx	; "gnome-calculator\0"&"gnome-calculator\0"
+	pop	ebx		; "gnome-calculatorABBBBCCCC"
+	mov	[ebx+16],al	; "gnome-calculator\0BBBBCCCC
+	mov	[ebx+17],ebx	; "gnome-calculator\0"&"gnome-calculator\0CCCC
 	mov	[ebx+21],eax	; "gnome-calculator\0"&"gnome-calculator\0"\0\0\0\0
 	lea	ecx,[ebx+17]	; cargamos [ebx+17] como segundo argumento
 	lea	edx,[ebx+21]	; cargamos [ebx+33] como tercer argumento
@@ -16,7 +16,6 @@ _shellcode:
 	mov	al,1
 	xor	ebx,ebx
 	int	0x80		; salida limpia
-
-_shellcode_get_cmd:
-	call	_shellcode
+_shellcd_db:
+	call	_shellcd
 	db	"gnome-calculatorABBBBCCCC"
