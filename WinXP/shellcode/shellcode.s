a24aba56e4dfe12d75fe5d9feea9fa8a540ae539
Daniel Rodriguez pueyo <danrodri@c1r16s3.42madrid.com>
2022-07-26 18:18:53 +0200

    Linux: works & WinXP: new shellcode to test

--- WinXP/shellcode/shellcode.s
+++ WinXP/shellcode/shellcode.s
@@ -1,13 +1,11 @@
+
 	section	.text
 
 _payload:
 	xor	eax,eax
-	push	eax		; null byte
-	mov	ebx,0x6578652e	; '.exe'...
-	push	ebx		; ...a la pila
+	push	eax		; null byte && uCmdShow
 	mov	ebx,0x636c6163	; 'calc'...
 	push	ebx		; ..a la pila
-	mov	ebx,esp		; ptr a 'calc.exe' en la pila
-	push	eax		; uCmdShow  = 0
+	mov	ebx,esp		; ptr a 'calc' en la pila
 	push	ebx	   	; lpCmdLine = PTR 'calc.exe'
 	call	0x7c8623ad	; call ptr to  WinExec (check with arwin.exe)
